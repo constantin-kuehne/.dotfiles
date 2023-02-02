21a7c8ae4df219451819d2027f6b28515173da01
Constantin Kühne <constantin.kuehne@student.hdbw-hochschule.de>
2023-02-02 18:22:30 +0100

    updated nvim-tree config for new version

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -31,12 +31,6 @@ nvim_tree.setup {
   },
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   actions = {
     open_file = {
        quit_on_open = true,
@@ -92,3 +86,27 @@ nvim_tree.setup {
     require_confirm = true,
   },
 }
+
+
+local function open_nvim_tree(data)
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
