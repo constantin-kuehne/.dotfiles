db61eb48641a3ba786f53d9d4d1d11424157eb14
Constantin Kühne <constantin.kuehne@student.hdbw-hochschule.de>
2023-08-18 23:59:03 +0200

    added keymaps for fugitive

--- nvim/.config/nvim/after/plugin/keymap/neogit.lua
+++ nvim/.config/nvim/after/plugin/keymap/neogit.lua
@@ -2,8 +2,8 @@ local Remap = require("const.keymap")
 local nnoremap = Remap.nnoremap
 local neogit = require('neogit')
 
-nnoremap("<leader>gs", function()
-    neogit.open({ })
-end);
+-- nnoremap("<leader>gs", function()
+--     neogit.open({ })
+-- end);
 
 nnoremap("<leader>ga", "<cmd>!git fetch --all<CR>");
